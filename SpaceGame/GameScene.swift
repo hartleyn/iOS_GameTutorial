de5a60c45f35a16a9b7347bd1e792fd89b0c6901
Nicholas Hartley <nhartley18@me.com>
2016-12-13 11:26:48 -0500

    iOS Game Tutorial - First commit

---
+++ SpaceGame/GameScene.swift
@@ -0,0 +1,56 @@
+//
+//  GameScene.swift
+//  SpaceGame
+//
+//  Created by Nicholas Hartley on 12/12/16.
+//  Copyright © 2016 Nicholas Hartley. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var starfield:SKEmitterNode!
+    var player:SKSpriteNode!
+    
+    var scoreLabel:SKLabelNode!
+    var score:Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    override func didMove(to view: SKView) {
+        
+        starfield = SKEmitterNode(fileNamed: "Starfield")
+        starfield.position = CGPoint(x: 0, y: 667)
+        starfield.advanceSimulationTime(10)
+        self.addChild(starfield)
+        
+        starfield.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "shuttle")
+        
+        player.position = CGPoint(x: self.frame.size.width / 2, y: player.size.height / 2 + 15)
+        
+        self.addChild(player)
+        
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
+        
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: 0, y: self.frame.size.height - 40)
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        scoreLabel.fontSize = 36
+        scoreLabel.fontColor = UIColor.white
+        score = 0
+        
+        self.addChild(scoreLabel)
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
